8472bfe3996734c0cd729c8cee40eba828ee2aba
hairjd2 <hairjd2@gmail.com>
2022-06-06 23:19:07 -0400

    Added JSON parser library

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+#include <cstdlib>
+#include <string>
+#include <fstream>
 #include <iostream>
+#include <json\json.h>
 #include "Food_Item.h"
 
+void parseItems();
+
 int main() {
+    Food_Item apple = parseItems();
+    apple.listProperties();
+    return 0;
+}
+
+Food_Item parseItems() {
     Food_Item apple("apple", 100);
     apple.setFiber(4);
     apple.setCarbs(14);
-    apple.listProperties();
-    return 0;
 }
